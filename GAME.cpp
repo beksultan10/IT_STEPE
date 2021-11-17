86b8740310a2e3ce6a97ebab7d6907977b044678
beksultan10 <94545033+beksultan10@users.noreply.github.com>
2021-11-17 21:25:43 +0600

    Update GAME.cpp

--- GAME.cpp
+++ GAME.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
-
-
 int main() {
 	setlocale(LC_ALL, "Russian");
 	std::cout << "Magic" << std::endl;
 	std::cout << "Дамблдор" << std::endl;
+	std::cout << "volend" << std::endl;
 }
